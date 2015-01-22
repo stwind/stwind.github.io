9818842e3c4a481b1bc4732f630c1331f9535a43
stwind <stwindfy@gmail.com>
2015-01-22 08:59:57 +0800

    Update gulpfile and src

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,17 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var $log = $.util.log;
 
+var gulpsmith = require('gulpsmith');
+var markdown = require('metalsmith-markdown');
+
+gulp.task('clean', require('del')
+          .bind(null, ['.tmp', 'dist']));
+
 gulp.task('default', function () {
+  return gulp.src('src/nodes/*.md')
+  .pipe(
+    gulpsmith()
+    .use(markdown())
+  )
+  .pipe(gulp.dest('./build'))
 });
