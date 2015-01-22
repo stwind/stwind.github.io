493627b35035f3bc714a95b70676dd2b5ebd7ee0
stwind <stwindfy@gmail.com>
2015-01-22 09:19:50 +0800

    Fix markdown front matter handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 'use strict';
 
 var gulp = require('gulp');
+var _ = require('lodash');
 var $ = require('gulp-load-plugins')();
 var $log = $.util.log;
-
 var gulpsmith = require('gulpsmith');
 var markdown = require('metalsmith-markdown');
 
-gulp.task('clean', require('del')
-          .bind(null, ['.tmp', 'dist']));
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('default', function () {
   return gulp.src('src/nodes/*.md')
+
+  .pipe($.frontMatter()).on('data', function(file){
+    _.assign(file, file.frontMatter);
+    delete file.frontMatter;
+  })
+
   .pipe(
     gulpsmith()
     .use(markdown())
   )
-  .pipe(gulp.dest('./build'))
+
+  .pipe(gulp.dest('.tmp/nodes'))
 });
