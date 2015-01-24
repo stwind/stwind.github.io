8a6d77c271f5d56258079133669e420f1c038259
stwind <stwindfy@gmail.com>
2015-01-24 15:04:31 +0800

    Use webpack-dev-server and related react things

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,35 @@ var $log = $.util.log;
 var gulpsmith = require('gulpsmith');
 var markdown = require('metalsmith-markdown');
 
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('./webpack.config.js');
+
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('webpack', function () {
+  return gulp.src('src/scripts/main.js')
+  .pipe($.webpack(webpackConfig))
+  .pipe(gulp.dest('.tmp/scripts'));
+});
+
+gulp.task('webpack-dev-server', function (callback) {
+  var compiler = webpack(webpackConfig),
+      port = 8080;
+
+  new WebpackDevServer(compiler, {
+    contentBase: 'src/',
+    hot: true,
+    publicPath: '/assets/',
+    port: port
+  })
+  .listen(port, 'localhost', function (err){
+    if (err) $log('[webpack-dev-server] error', err);
+
+    $log('[webpack-dev-server]', 'http://localhost:8080/webpack-dev-server/');
+    require('opn')('http://localhost:8080/webpack-dev-server/');
+  });
+});
 
 gulp.task('default', function () {
   return gulp.src('src/nodes/*.md')
@@ -22,5 +50,5 @@ gulp.task('default', function () {
     .use(markdown())
   )
 
-  .pipe(gulp.dest('.tmp/nodes'))
+  .pipe(gulp.dest('.tmp/nodes'));
 });
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+'use strict';
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+
+  output: {
+    path: '/assets/',
+    publicPath: '/assets/',
+    filename: "[name].js"
+  },
+
+  entry: [
+    'webpack/hot/only-dev-server',
+    './src/scripts/main.js'
+  ],
+
+  resolve: {
+    extensions: ['', '.js']
+  },
+
+  cache: true,
+  debug: true,
+
+  stats: {
+    colors: true,
+    reasons: true
+  },
+
+  module: {
+    preLoaders: [{
+      test: '\\.js$',
+      exclude: 'node_modules',
+      loader: 'jshint'
+    }],
+    loaders: [{
+        test: /\.js$/,
+        loaders: ['react-hot','jsx?harmony']
+      }, {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!less-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }, {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
+      }]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+
+};
