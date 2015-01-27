2671745af8207efe48934068429e68910d63c2c1
stwind <stwindfy@gmail.com>
2015-01-27 11:46:17 +0800

    Separate routes to be standalone module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,17 +2,8 @@
 
 var React = require('react');
 var Router = require('react-router');
-var { Route, DefaultRoute, Redirect, Link } = Router;
 
-var App = require('./app');
-var Post = require('./post');
-
-var routes = (
-  <Route path="/" handler={App}>
-    <Route name="node" path="p/:id" handler={Post}/>
-    <Redirect from="/" to="node" params={{id: 'about'}} />
-  </Route>
-);
+var routes = require('./routes');
 
 Router.run(routes, function (Handler) {
   React.render(<Handler/>, document.getElementById('content'));
---
+++ src/scripts/routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var React = require('react');
+var Router = require('react-router');
+var { Route, DefaultRoute, Redirect, Link } = Router;
+
+var App = require('./app');
+var Post = require('./post');
+
+var routes = (
+  <Route path="/" handler={App}>
+    <Route name="node" path="p/:id" handler={Post}/>
+    <Redirect from="/" to="node" params={{id: 'about'}} />
+  </Route>
+);
+
+module.exports = routes;
