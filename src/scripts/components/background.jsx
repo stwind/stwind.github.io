94b0d729a20d086893868be4e954d969007ed9b1
stwind <stwindfy@gmail.com>
2015-07-07 21:39:10 +0800

    update background

--- src/scripts/components/background.jsx
+++ src/scripts/components/background.jsx
@@ -36,15 +36,19 @@ export default function background (responses) {
 }
 
 class Cell {
-  constructor(x, y, radius) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
+  constructor(x, y, mass) {
+    this.reset(x, y);
+    this.mass = mass;
   }
 
   update(vx, vy) {
-    this.x += vx;
-    this.y += vy;
+    this.x += vx / this.mass;
+    this.y += vy / this.mass;
+  }
+
+  reset(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
 
@@ -54,44 +58,67 @@ function makeSkecth(props) {
 
     p.setup = function setup(){
       p.createCanvas(props.width, props.height);
-      p.background(255);
       p.smooth();
+      p.angleMode(p.RADIANS);
+      p.frameRate(30);
       generateCells();
       cells.forEach(drawCell);
     }
 
     p.draw = function draw(){
-      p.background(255);
+      p.background(203,51,51);
+      // p.background(247, 247, 247);
       var speed = getSpeed();
-      cells.forEach(cell => updateCell(cell, speed));
+      cells.forEach(cell => {
+        updateCell(cell, speed);
+        drawCell(cell);
+      });
       updateCounter();
     }
 
     function drawCell (cell) {
       p.noStroke();
-      p.fill(203,51,51,120);
-      p.ellipse(cell.x, cell.y, cell.radius * 2, cell.radius * 2);
+      p.fill(183, 40, 46, 60);
+      // p.fill(229, 229, 229, 60);
+      var radius = p.random(cell.mass * 1.9, cell.mass * 2.1);
+      p.ellipse(cell.x, cell.y, radius, radius);
     }
 
-    function updateCell (cell, speed) {
-      cell.update(p.random(-speed, speed), p.random(-speed, speed));
-      drawCell(cell);
+    function updateCell (cell, { vx, vy }) {
+      // cell.update(p.random(-speed, speed), p.random(-speed, speed));
+      cell.update(vx, vy);
+
+      if (cell.x + cell.mass < 0) {
+        cell.reset(p.width + cell.mass, p.random(p.height));
+      }
+      else if (cell.x > p.width + cell.mass) {
+        cell.reset(-cell.mass, p.random(p.height));
+      }
+      else if (cell.y < - cell.mass) {
+        cell.reset(p.random(p.width), p.height + cell.mass);
+      }
+      else if (cell.y > p.height + cell.mass) {
+        cell.reset(p.random(p.width), -cell.mass);
+      }
     }
 
     function getSpeed () {
-      return counter < props.frameFast ? props.speedSlow: props.speedFast;
+      // var speed = counter < props.frameFast ? props.speedSlow: props.speedFast;
+      var speed = props.speedSlow;
+      var vx = p.cos(p.radians(props.angle)) * speed,
+          vy = p.sin(p.radians(props.angle)) * speed;
+      return { vx, vy };
     }
 
     function updateCounter() {
-      counter++;
-      if (counter >= props.frameMax) counter = 0;
+      if (++counter >= props.frameMax) counter = 0;
     }
 
     function generateCells(){
       for(var i = 0; i < props.numCell; i++){
         var x = p.random(p.width);
         var y = p.random(p.height);
-        var rad = p.random(props.minRad, props.maxRad);
+        var rad = p.random(props.minMass, props.maxMass);
 
         cells.push(new Cell(x, y, rad));
       }
@@ -106,9 +133,10 @@ Hook.prototype.hook = function hook(node) {
   var props = {
     width: node.clientWidth,
     height: node.clientHeight,
-    speedSlow: 1, speedFast: 10,
+    speedSlow: 300, speedFast: 1500,
     frameMax: 45, frameFast: 40,
-    minRad: 10, maxRad: 90,
+    minMass: 10, maxMass: 70,
+    angle: 330,
     numCell: 350
   };
 
