12450d5435f51e80d9e64c6057ee0d3c8f5632f0
stwind <stwindfy@gmail.com>
2015-07-07 11:09:44 +0800

    update p5 sketch

--- src/scripts/components/backgrond.jsx
+++ src/scripts/components/backgrond.jsx
@@ -37,19 +37,72 @@ export default function background (responses) {
 
 function makeSkecth(props) {
   return function sketch (p) {
+    var cellNumber = 350;
+    var speed = 1;
+    var speedFast = 10;
+    var frameMax = 45;
+    var frameFast = 40;
+    var minRad = 10;
+    var maxRad = 90;
 
-    var gray = 0;
-    var h = 10; 
+    var _cellArr = [];
 
-    p.setup = function() {
-      var cnv = p.createCanvas(props.width, props.height);
-      p.rectMode(p.CENTER);
-    };
+    p.setup = function setup(){
+      p.createCanvas(props.width, props.height);
+      p.background(255);
+      p.smooth();
+      generateCells();
+    }
 
-    p.draw = function() {
-      p.background(gray);
-      p.rect(p.width/2, p.height/2, h, h);
-    };
+    p.draw = function draw(){
+      p.background(255);
+      for(var i=0; i < _cellArr.length; i++){
+        var cell = _cellArr[i];
+        cell.update();
+      }
+    }
+
+    function generateCells(){
+      for(var i=0; i < cellNumber; i++){
+        var cell = new BloodCell();
+        cell.genarate();
+        _cellArr.push(cell);
+      }
+    }
+
+    function BloodCell() {
+      var x,y,vx,vy,rad;
+      this.counter = 0;
+      x = p.random(p.width);
+      y = p.random(p.height);
+      rad = p.random(minRad,maxRad);
+      vx = p.random(-speed,speed);
+      vy = p.random(-speed,speed);
+
+      this.genarate = function(){
+        p.noStroke();
+        p.fill(203,51,51,120);
+        p.ellipse(x,y,rad*2,rad*2);
+      }
+
+      this.update = function()
+      {
+        if(this.counter < frameFast){
+          vx = p.random(-speed,speed);
+          vy = p.random(-speed,speed);
+        }else{
+          vx = p.random(-speedFast,speedFast);
+          vy = p.random(-speedFast,speedFast);
+        }
+        this.counter++;
+        if(this.counter == frameMax)this.counter = 0;
+
+        x += vx;
+        y += vy;
+
+        this.genarate();
+      }
+    }
   };
 }
 
