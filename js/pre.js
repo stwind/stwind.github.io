878dcfceb871bd03d0813c70b81c33b3aa0e678a
stwind <stwindfy@gmail.com>
2015-10-22 10:19:34 +0800

    Site updated at 2015-10-22T10:19:34

--- js/pre.js
+++ js/pre.js
@@ -1,65 +1,64 @@
 (function (window) {
   var pre = {},
-      chars = "的就而回就傷是你選了是憶越擇越人甜",
+      chars = [
+        ["l ", "L ", "ﾚ "],
+        ["o ", "O ", "0 ", "再"],
+        ["a ", "A ", "α ","σ ", "Λ "],
+        ["d ","ｄ", "D ", "∂ "],
+        ["i ","ｉ","I ","Ｉ", "見"],
+        ["n ", "N "],
+        ["g ", "G "]
+      ],
+      seq = [0, 0, 0, 0, 0, 0, 0],
+      fpsInterval = 1000/5, 
+      then = Date.now(), now = then, elapsed,
       $pre = document.getElementById('pre'),
       $msg = document.getElementById('msg'),
       started = false;
 
-  function randChar() {
+  function randChar (chars) {
     var i = Math.floor(Math.random() * chars.length);
     return chars[i];
   }
 
-  function randText (len) {
+  function genText () {
+    console.log(seq);
     var text = '';
-    for (var i = 0; i < len; i++) {
-      text += randChar();
+    for (var i = 0; i < seq.length; i++) {
+      text += chars[i][seq[i]];
     }
     return text;
   }
 
   function updateText () {
-    var text = randText(2) + '再' + randText(1) + '，' + 
-               randText(7) + '見' + randText(3) + '。';
-    $msg.textContent = text;
+    var i = Math.floor(Math.random() * seq.length);
+    seq[i] = Math.floor(Math.random() * chars[i].length);
+    $msg.textContent = genText();
   }
+  
+  function animate () {
+    if (started) window.requestAnimationFrame(animate);
 
-  function finalText1 () {
-    var text = '　　再　　　　　　　　　見　　　。';
-    $msg.textContent = text;
-  }
+    now = Date.now();
+    elapsed = now - then;
 
-  function finalText2 () {
-    var text = '　　　　　　　　　　　　　　　　。';
-    $msg.textContent = text;
-  }
-
-  function step () {
-    updateText();
-    if (started) {
-      window.requestAnimationFrame(step);
-    }
+    if (elapsed > fpsInterval)
+      then = now - (elapsed % fpsInterval);
+      updateText();
   }
       
   pre.start = function start () {
     started = true;
-    step();
+    animate();
   };
 
   pre.stop = function stop (done) {
     setTimeout(function () {
-      started = false;
-      setTimeout(function () { updateText(); }, 200);
-      setTimeout(function () { updateText(); }, 500);
-      setTimeout(function () { updateText(); }, 900);
-      setTimeout(function () { updateText(); }, 1400);
-      setTimeout(function () { finalText1(); }, 2000);
-      setTimeout(function () { finalText2(); }, 2800);
+      $pre.parentNode.removeChild($pre);
       setTimeout(function () {
-        $pre.parentNode.removeChild($pre);
         done();
-       }, 3800);
-    }, 1000);
+      }, 300);
+    }, 1200);
   };
 
   window.pre = pre;
